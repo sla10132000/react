f25da99e88572a3910399e045a6e573fd10a0c07
sla10132000@yahoo.co.jp <sla10132000@yahoo.co.jp>
2018-07-31 21:43:21 +0900

    a

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ import { routerReducer, routerMiddleware } from 'C:/Users/Administrator/AppData/
 import logger from 'C:/Users/Administrator/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/redux-logger';
 import tasksReducer from '../reducers/tasks';
 //import testsReducer from '../reducers/tests';
-//
+////
 // historyはsrc/index.jsから渡すようにする
 export default function createStore(history) {
   return reduxCreateStore(
